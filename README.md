8f63a692da4f794dff2d6a1ac79d19da37b0c341
Matheus da Mata <matheusdamatag@gmail.com>
2022-09-21 22:07:30 -0300

    added user creation

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# node-mongo
+Node + Typescript + Mongo com estrutura MVC aplicada.
+
+### Pré-requisitos globais:
+`npm i -g nodemon typescript ts-node`
+
+### Instalação
+`npm install`
+
+### Para rodar o projeto
+`npm run start-dev`
---
+++ src/controllers/userController.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from 'express';
+
+import User from '../models/User';
+
+export const nome = (req: Request, res: Response) => {
+    let nome: string = req.query.nome as string;
+    let idade: string = req.query.idade as string;
+
+    res.render('pages/nome', {
+        nome,
+        idade
+    });
+};
+
+export const addUserAction = async (req: Request, res: Response) => {
+    let user = req.body;
+    let interests = user.interests.split(" ");
+
+    let newUser = new User();
+    newUser.name = {
+        firstName: user.firstName,
+        lastName: user.lastName
+    };
+    newUser.email = user.email;
+    newUser.age = parseInt(user.age);
+    newUser.interests = interests;
+
+    if(user) {
+        await newUser.save();
+        console.log("User cadastred!");
+    } else {
+        console.log("Unregistered user.");
+    }
+
+    res.redirect('/');
+};
+
+export const idadeForm = (req: Request, res: Response) => {
+    res.render('pages/idade');
+};
+
+export const idadeAction = (req: Request, res: Response) => {
+    let mostrarIdade: boolean = false;
+    let idade: number = 0;
+
+    if(req.body.ano) {
+        let anoNascimento: number = parseInt(req.body.ano as string);
+        let anoAtual: number = new Date().getFullYear();
+        idade = anoAtual - anoNascimento;
+        mostrarIdade = true;
+    }
+
+    res.render('pages/idade', {
+        idade,
+        mostrarIdade
+    });
+};
